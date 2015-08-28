c6a950976a267fb0fc81e8d342fde136700d0cc5
Pedro Ramirez <pedro.rmrz.srz@gmail.com>
2015-08-28 08:31:37 -0500

    added validations to the ToDo backend

--- TODOServer/Program.cs
+++ TODOServer/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,40 @@ namespace TODOServer
     {
         static void Main(string[] args)
         {
-            DiPSClient client = new DiPSClient (ConfigurationManager.AppSettings["dipsserver"]);
+            var dipsRunning = false;
+            while (true)
+            {
+                
+                    Process[] processlist = Process.GetProcesses();
+                    foreach (Process theprocess in processlist)
+                    {
+                        if (theprocess.ProcessName.ToLower().Trim().Contains("dips"))
+                        {
+                            dipsRunning = true;
+                            break;
+                        }
+                    }
+                    if (dipsRunning)
+                        break;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Seems like the DiPS server is not running, please make sure that the server is running.");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Press enter to retry, or another key to exit");
+                    if (Console.ReadKey().Key != ConsoleKey.Enter)
+                        Environment.Exit(1);
+            }
+
+            DiPSClient client = new DiPSClient(ConfigurationManager.AppSettings["dipsserver"]);
+            
 
             TaskController tasks = new TaskController(client);
             UserController users = new UserController(client);
-
+            
             while (true)
             {
-
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("ToDo backend");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("Type exit to finish this process");
                 var exit = Console.ReadLine();
                 if (exit.ToLower() == "exit")
